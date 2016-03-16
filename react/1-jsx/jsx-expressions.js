edc2b8a5496de887dd838f3f358dd5c0c5b006cc
Bart Waardenburg <bart.waardenburg@incentro.com>
2016-03-16 23:49:35 +0100

    JSX training part added

---
+++ react/1-jsx/jsx-expressions.js
@@ -0,0 +1,37 @@
+// 2. JSX
+var name = 'Incentro';
+
+var disabled = false;
+
+var todos = [
+  'learn react',
+  'learn webpack',
+  'learn redux'
+];
+
+var objectSpread = {
+	className: 'fancy-pancy-paragraph',
+	id: 'nice'
+};
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      // Gotchas on class and for => className and htmlFor
+      // Use curly braces (block scoping) to use javascript expressions
+      <div>
+        <p className="fancy-pancy-paragraph">{name} {name === 'Incentro' ? <input value={name} disabled={disabled}/> : 'is the best'}</p>
+
+		{/* use map/filter/reduce array functions to render lists */}
+        <ul>
+          {todos.map(todo => <li>{todo}</li>)}
+        </ul>
+
+		{/* use the new object spread operator to assign multiple props or attributes */}
+		<div {...objectSpread}>Object spread operator</div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<HelloWorld date={new Date()} />, document.body);
---
+++ react/1-jsx/jsx.js
@@ -0,0 +1,35 @@
+// 1. JSX
+
+// React is used to create and maintain the a representation of data (in this case it uses the DOM)
+// React works with components
+// Every part of the page is a component in the same way you will think of components using CSS
+const HelloWorld = React.createClass({
+
+  // The Render function is the piece of the React API that will turn javascript into HTML
+  render: function() {
+    return (
+      <p>
+        Hello, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()} <NonJSX />
+      </p>
+    );
+  }
+});
+
+// JSX makes that you can write your HTML in a familiar way
+const NonJSX = React.createClass({
+  render: () =>
+    React.createElement('a', {href: 'https://facebook.github.io/react/'}, 'Hello!')
+});
+
+// React is seperate to the ReactDom library
+// In this case we're using the DOM to display our data
+// But we could easily take something else like canvas, webGL or any other format to render our JSX
+
+// The setInterval will run the ReactDOM.render function every .5 seconds and it will use a smart diffing algorithm to calculate the minimal mutation
+setInterval(function() {
+  ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.body
+  );
+}, 500);
