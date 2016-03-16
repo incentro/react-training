5c2ef38d1dce27573973904c7e27311e258919ad
Bart Waardenburg <bart.waardenburg@incentro.com>
2016-03-17 00:48:06 +0100

    React Api completed

--- react/3-composition/1-multiple-components.js
+++ react/3-composition/1-multiple-components.js
@@ -1,12 +1,42 @@
 // Seperate the interface into multiple components
 // Seperation of concerns by components and not by style and interactivity
 var Avatar = React.createClass({
+  getInitialState: function() {
+    return {
+      pagename: 'Incentro'
+    }
+  },
+
+  // Pass down a function to handle changes down the DOM
+  handleUserInput: function(pagename) {
+    this.setState({
+      pagename: pagename
+    });
+  },
+
   render: function() {
     return (
       <div>
-        <PagePic pagename={this.props.pagename} />
-        <PageLink pagename={this.props.pagename} />
+        <PagePic pagename={this.state.pagename} />
+        <PageLink pagename={this.state.pagename} />
+        {/* Pass down the handleUserInput to the child component to handle the search */}
+        <SearchBar pagename={this.state.pagename} onUserInput={this.handleUserInput}/>
       </div>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+
+  // Create a function that catches the value of the input and passes it back up to where the state needs to be changed
+  handleChange: function() {
+    this.props.onUserInput(
+      this.refs.searchbar.value
+    );
+  },
+  render: function() {
+    return (
+      <input type="text" value={this.props.pagename}  ref="searchbar" onChange={this.handleChange}/>
     );
   }
 });
@@ -32,7 +62,5 @@ var PageLink = React.createClass({
   }
 });
 
-ReactDOM.render(
-  <Avatar pagename="Incentro" />,
-  document.body
-);
+
+ReactDOM.render(<Avatar/>, document.body);
