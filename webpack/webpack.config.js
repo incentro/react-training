a042902e6ee43089cedb9cbadb92b46015bbca78
niels_aulman <niels.aulman@incentro.com>
2016-03-17 14:39:49 +0100

    (feat) add working reloading config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,36 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        __dirname + '/src/asset/javascript/app.js',
+	entry : __dirname + '/src/asset/javascript/app.js',
+	output: {
+		path : __dirname + '/dist/js/',
+		filename: 'bundle.js'
+	},
+	module : {
+		loaders : [
+			{
+				test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+			}
+		]
+	},
+	plugins : [
+		new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.tmpl.html"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+
+
     ],
-    output: {
-        path: __dirname + '/dist/asset/javascript',
-        filename: 'app.js'
+    devServer : {
+        contentBase: "./dist",
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        host: '0.0.0.0',
+        port: 1337
     }
 }
